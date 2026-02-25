7476fac42d317d454adaf4d77e1c58f227acb1b6
Aleksei Shlykov <shlikovalexei@gmail.com>
2026-02-25 11:33:35 +0100

    Fix

--- hobby-finder/src/app/page.tsx
+++ hobby-finder/src/app/page.tsx
@@ -1,12 +1,10 @@
 import ClientProvider from '@/components/ClientProvider';
 import TestFlow from '@/components/TestFlow';
-import DebugRestart from '@/components/DebugRestart';
 
 export default function Home() {
   return (
     <ClientProvider>
       <TestFlow />
-      <DebugRestart />
     </ClientProvider>
   );
 }
